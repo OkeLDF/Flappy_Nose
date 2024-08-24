67ca222a98db62ff5ddd51cb9d0bb3661da1eb45
OkeLDF <otaviofolador43@gmail.com>
2024-08-23 23:47:32 -0300

    Score added

--- flappy.py
+++ flappy.py
@@ -7,8 +7,9 @@ face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 color = (0, 255, 0) # rgb
 border_color = (0, 0, 255)
 circle_r = 30
-# x_speed = -8
 x_speed = -12
+score = 0
+score_enabled = True
 
 screen_w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 screen_h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
@@ -78,10 +79,8 @@ def warning(msg:str):
 obstacle = Obstacle()
 
 def detect_face(frame):
-    global face_cascade
-    global border_color
-    global color
-    global obstacle
+    global face_cascade, border_color, color, obstacle, score, score_enabled
+
     face_img = frame.copy()
     face_img = cv.cvtColor(face_img, cv.COLOR_RGB2BGR)
 
@@ -91,8 +90,17 @@ def detect_face(frame):
 
         if obstacle.touching((coord[0], coord[1])):
             color = (255, 0, 0)
+            score=0
+            score_enabled = False
         else:
             color = (0, 255, 0)
+
+        if coord[0] < obstacle.get_x() + 6 and coord[0] >= obstacle.get_x() - 6:
+            score+=1
+            score_enabled = False
+
+        if obstacle.get_x() <= line1[0][0]:
+            score_enabled = True
 
         if(coord[0]>line1[0][0] and coord[0]<line2[0][0]):
             cv.circle(face_img, coord, circle_r, color, 5)
@@ -100,6 +108,7 @@ def detect_face(frame):
     cv.line(face_img, line1[0], line1[1], border_color, 3)
     cv.line(face_img, line2[0], line2[1], border_color, 3)
     obstacle.draw(face_img)
+    cv.putText(face_img, str(score), (20, 90), cv.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 3)
 
     return cv.cvtColor(face_img, cv.COLOR_BGR2RGB)
 
@@ -112,7 +121,7 @@ while cap.isOpened():
         break
 
     frame_face = detect_face(frame)
-    cv.imshow("Webcam Feed", frame_face)
+    cv.imshow("Flappy Nose", frame_face)
 
     obstacle.addto_x(x_speed)
     if obstacle.superior_pillar[0] + obstacle.superior_pillar[2] < 0:
